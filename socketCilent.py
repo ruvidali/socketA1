cb3b363784f542f0cc3d0fe876479ae978a4480d
ruvidali <ruvidali@proton.me>
2026-04-12 14:51:00 +1200

    Changes

--- socketCilent.py
+++ socketCilent.py
@@ -1,1 +1,20 @@
 import socket
+import os
+from tdqm import tdqm
+
+
+def run_client():
+    host = "127.0.0.1"
+    port = 5000
+    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    client_socket.connect((host, port))
+
+    while True:
+        print(
+            "Connection Successful, \ntype ls for file list, type filename to download"
+        )
+        choice = input("Enter command")
+
+        client_socket.send(choice.encode())
+        response = client_socket.recv(4096).decode()
+        print(f"Directory: {response}")
--- socketServer.py
+++ socketServer.py
@@ -1,1 +1,26 @@
 import socket
+import os
+from tdqm import tdqm
+
+
+def run_server():
+    # Server setup
+    host = "127.0.0.1"
+    port = 5000
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind((host, port))
+    server_socket.listen(1)
+    print(f"Sever started, Listening on host: {host}, port: {port}")
+
+    conn, addr = server_socket.accept()
+    with conn:
+        print(f"Connected to: {addr}")
+        while True:
+            command = conn.recv(1024).decode()
+            if not command:
+                break
+            # Client should be able to check the directory for the file
+            # Or or just type the name of the file to download
+            if command == "ls":
+                files = os.listdir()
+                conn.send(files.encode())
